e25f2bda946dc8cc77615b4ea16483127ec18ced
Fererra <nikita.sukhorukoff@gmail.com>
2023-10-05 07:52:15 +0300

    Refactoring

--- app.js
+++ app.js
@@ -5,24 +5,19 @@ const bodyParser = require('body-parser');
 // var logger = require('winston');
 
 var indexRouter = require('./routes/index');
-var signUpRouter = require('./routes/sign-up');
 var loginRouter = require('./routes/login');
+var signUpRouter = require('./routes/signup');
 
 var app = express();
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'pug');
-
-// app.use(logger('dev'));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/views'));
 
 app.use('/', indexRouter);
-app.use('/signup', signUpRouter);
 app.use('/login', loginRouter);
+app.use('/signup', signUpRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -34,7 +29,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  
   // render the error page
   res.status(err.status || 500);
   res.sendFile(__dirname + '/views/error.html');
